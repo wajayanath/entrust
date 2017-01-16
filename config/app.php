41cb665fbeb931c9fe5b5702bb08cb6141041486
Jayanath Kumara <wajayanath@gmail.com>
2017-01-16 16:11:10 +0530

    apply moderation

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Zizaco\Entrust\EntrustServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Hootlex\Moderation\ModerationServiceProvider::class,
 
     ],
 
